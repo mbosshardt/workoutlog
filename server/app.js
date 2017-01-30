20a41e4a4c39d1c01c9b4f26492abca70dbe8086
Morgan Bosshardt <morgan.bosshardt@gmail.com>
2017-01-30 14:57:58 -0500

    added connection to postgres

--- server/app.js
+++ server/app.js
@@ -1,5 +1,60 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("workoutlog", "postgres" ,"LetMeIn1234!", {
+	host 	: "localhost",
+	dialect : "postgres"
+});
+
+sequelize.authenticate().then(
+	function() {
+		console.log("connected to workoutlog postgres db");
+	},
+	function (err) {
+		console.log(err);
+	}
+);
+
+// build a user model in sqllize
+var User = sequelize.define("user", {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING
+});
+
+// creates the table in postgres
+// matches the model we defined
+// doesn't drop the db
+User.sync();
+// User({ force: true}); //drops the table completely
+
+app.use(bodyParser.json());
+
+app.post("/api/user", function(req, res) {
+	// when we post to api ser, it will want a user object in the body
+	var username = req.body.user.username;
+	var pass = req.body.user.password;		// TODO: has this password - HASH = not human readable
+	// need to create a user object and use sequelize to put that user into our database
+
+	// match the model we created above
+	// Sequelize - take the user model and go out to the db and create this:
+	User.create({
+		username: username,
+		passwordhash: ""
+	}).then(
+		// Sequelize is going to return the object it created from db
+		function createSuccess(user){
+			res.json({
+				user: user,
+				message: "create"
+			});
+		},
+		function createError(err){
+			res.send(500, err.message);
+		}
+	);
+});
 
 app.use(require("./middleware/headers"));
 
